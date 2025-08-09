84d421356cdc33ba0efd889ff0e8f7205a6f6318
Dsinkerii <104655906+dsinkerii@users.noreply.github.com>
2025-08-09 20:45:14 +0500

    backup: updated room gen

--- Assets/Scripts/MeshGen.cs
+++ Assets/Scripts/MeshGen.cs
@@ -1,73 +1,233 @@
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.ProBuilder;
 using UnityEngine.ProBuilder.MeshOperations;
 
 public class MeshGen : MonoBehaviour
 {
-    public Material defaultMaterial;
-    public GameObject masterMesh;
     public static MeshGen Instance;
-    void Start(){
-        Instance = this;
-    }
-    static readonly List<ProBuilderMesh> RawObjects = new();
-    public static void GenerateMesh(Corridor root)
-    {
-        GameObject masterMesh = Instance.masterMesh;
-        GenerateMeshes(root, masterMesh.transform);
-        LocalCombineMeshes(masterMesh);
-    }
+    public Material floorMat, wallMat;
+    public int Space = 2;
+    public float wallThickness = 2;
+    void Start() => Instance = this;
+    public float vertexMergeDistance = 0.001f;
+    private static List<Room> corridors = new();
+    private static readonly List<ProBuilderMesh> meshes = new();
+    private static bool[,] floorMap;
 
-    static void LocalCombineMeshes(GameObject masterMesh){
-        CombineMeshes.Combine(RawObjects, masterMesh.GetComponent<ProBuilderMesh>());
-        //pbm.SetMaterial(pbm.faces,Instance.defaultMaterial); //apply to all
-    }
+    public static void GenerateMesh(Corridor rootRoom, Vector2Int size, float wallHeight){
+        corridors.Clear();
+        meshes.Clear();
+        CreateCorridors(rootRoom);
+        CreateFloor(rootRoom);
+        CreateWalls(rootRoom, size, wallHeight);
+        PostProcessMeshes();
 
-    static void GenerateMeshes(Corridor corridor, Transform parent){
-        if (corridor == null) return;
-
-        if (corridor is Room){
-            CreateRoomMesh(corridor, parent);
-        }
-
-        if (corridor.corridors != null){
-            foreach (var child in corridor.corridors)
-                GenerateMeshes(child, parent);
-        }
     }
 
-    static void CreateRoomMesh(Corridor corridor, Transform parent){
-        var room = ShapeGenerator.GenerateCube(
-            PivotLocation.Center,
-            new(corridor.Width, 3, corridor.Height)
-        );
-        
-        room.transform.position = new( // set pivot point to be in center
-            corridor.x + corridor.Width/2f,
-            1.5f,
-            corridor.y + corridor.Height/2f
-        );
-        
-        room.transform.parent = parent;
-        room.GetComponent<MeshRenderer>().sharedMaterial = Instance.defaultMaterial;
-        RawObjects.Add(room);
-
-        if (corridor is Room roomCorridor){
-            foreach (var door in roomCorridor.doors){
-                CreateDoorTunnel(roomCorridor, door, room.transform);
+    //for roomgenerator
+    public static Texture2D CreateMap(){
+        Vector2Int size = new(floorMap.GetLength(0),floorMap.GetLength(1));
+        Texture2D newTex = new(size.x, size.y){filterMode=FilterMode.Point};
+        for(int x = 0; x < size.x; x++){
+            for(int y = 0; y < size.y; y++){
+                newTex.SetPixel(x,y,floorMap[x,y] ? Color.white : Color.black);
             }
         }
+        newTex.Apply();
+        return newTex;
     }
 
-    static void CreateDoorTunnel(Room room, Door door, Transform parent){
-        float tunnelDepth = Corridor.Space + 2;
-        Vector3 size = new(door.Width,1.5f,door.Height);
-        Vector3 position = new(door.x,1.5f,door.y);
+    static void CreateCorridors(Corridor node){
+        if (node.IsLeaf) return;
+        CreateCorridors(node.left);
+        CreateCorridors(node.right);
+        
+        var room1 = GetRandomRoom(node.left);
+        var room2 = GetRandomRoom(node.right);
+        var p1 = new Vector2Int(Random.Range(room1.Pos.x, room1.Pos.x + room1.Size.x), Random.Range(room1.Pos.y, room1.Pos.y + room1.Size.y));
+        var p2 = new Vector2Int(Random.Range(room2.Pos.x, room2.Pos.x + room2.Size.x), Random.Range(room2.Pos.y, room2.Pos.y + room2.Size.y));
 
-        var tunnel = ShapeGenerator.GenerateCube(PivotLocation.Center, size);
-        tunnel.GetComponent<MeshRenderer>().sharedMaterial = Instance.defaultMaterial;
-        tunnel.transform.position = position;
-        tunnel.transform.parent = parent;
+        if(Dice()){
+            CreateHorizontalCorridor(p1.x, p2.x, p1.y);
+            CreateVerticalCorridor(p1.y, p2.y, p2.x);
+        } else {
+            CreateVerticalCorridor(p1.y, p2.y, p1.x);
+            CreateHorizontalCorridor(p1.x, p2.x, p2.y);
+        }
+    }
+
+    static void PostProcessMeshes(){
+        CombineObjects();
+    }
+
+    //////////////////////
+    /// help functions /// : GenerateMeshes
+    //////////////////////
+    
+    static bool Dice() => Random.value > 0.5f;
+    static Room GetRandomRoom(Corridor node) => node.IsLeaf ? node.room : GetRandomRoom(Dice() ? node.left : node.right);
+    static void CreateHorizontalCorridor(int x1, int x2, int y) => corridors.Add(new Room { Pos = new Vector2Int(Mathf.Min(x1, x2), y), Size = new Vector2Int(Mathf.Abs(x2 - x1) + Instance.Space, Instance.Space) });
+    static void CreateVerticalCorridor(int y1, int y2, int x) => corridors.Add(new Room { Pos = new Vector2Int(x, Mathf.Min(y1, y2)), Size = new Vector2Int(Instance.Space, Mathf.Abs(y2 - y1) + Instance.Space) });
+
+    //////////////////////
+    /// help functions /// : CreateFloor
+    //////////////////////
+
+    static void CreateFloor(Corridor rootNode){
+        var parent = new GameObject("Floor").transform;
+        parent.parent = RoomGenerator.Instance.BaseMesh.transform;
+        CreateFloorsForRooms(rootNode, parent);
+        foreach(var corridor in corridors) CreateFloorSection(corridor, parent);
+    }
+
+    static void CreateFloorsForRooms(Corridor node, Transform parent){
+        if(node == null) return;
+        if(node.IsLeaf && node.room != null) CreateFloorSection(node.room, parent);
+        else { CreateFloorsForRooms(node.left, parent); CreateFloorsForRooms(node.right, parent); }
+    }
+
+    static void CreateFloorSection(Room room, Transform parent){
+        var floor = ShapeGenerator.CreateShape(ShapeType.Cube).gameObject;
+        floor.transform.parent = parent;
+        floor.transform.position = new Vector3(room.Pos.x + room.Size.x/2f, 0, room.Pos.y + room.Size.y/2f);
+        floor.transform.localScale = new Vector3(room.Size.x, 0.1f, room.Size.y);
+        floor.GetComponent<MeshRenderer>().material = Instance.floorMat;
+        meshes.Add(floor.GetComponent<ProBuilderMesh>());
+    }
+
+    //////////////////////
+    /// help functions /// : CreateWalls
+    //////////////////////
+    
+    static void CreateWalls(Corridor rootRoom, Vector2Int size, float wallHeight){
+        var wallParent = new GameObject("Walls").transform;
+        wallParent.parent = RoomGenerator.Instance.BaseMesh.transform;
+        
+        floorMap = new bool[size.x, size.y];
+        bool[,] wallMap = new bool[size.x, size.y];
+        
+        MarkFloorAreas(rootRoom, floorMap, size);
+        foreach(var corridor in corridors) MarkFloorArea(corridor, floorMap, size);
+
+        for(int x = 0; x < size.x; x++)
+            for(int y = 0; y < size.y; y++)
+                if(!floorMap[x,y] && (IsAdjacentToFloor(x, y, floorMap, size) || IsBoundaryWall(x, y, floorMap, size)))
+                    wallMap[x, y] = true;
+
+        var wallSegments = FindWallSegments(wallMap, size);
+        foreach(var segment in wallSegments) CreateWallSegment(segment, wallParent, wallHeight);
+    }
+    
+    static List<WallSegment> FindWallSegments(bool[,] wallMap, Vector2Int size){
+        var segments = new List<WallSegment>();
+        bool[,] processed = new bool[size.x, size.y];
+        
+        for(int y = 0; y < size.y; y++){
+            for(int x = 0; x < size.x; x++){
+                if(wallMap[x, y] && !processed[x, y]) {
+                    var horizontalSegment = CreateHorizontalSegment(x, y, wallMap, processed, size);
+                    if(horizontalSegment != null){ segments.Add(horizontalSegment); continue; }
+                    
+                    var verticalSegment = CreateVerticalSegment(x, y, wallMap, processed, size);
+                    if(verticalSegment != null){ segments.Add(verticalSegment); continue; }
+                    
+                    // if neither worked, create single wall
+                    segments.Add(new WallSegment(new Vector2Int(x, y), new Vector2Int(x, y), true));
+                    processed[x, y] = true;
+                }
+            }
+        }
+        return segments;
+    }
+    
+    static WallSegment CreateHorizontalSegment(int startX, int startY, bool[,] wallMap, bool[,] processed, Vector2Int size) {
+        int endX = startX;
+        while(endX + 1 < size.x && wallMap[endX + 1, startY] && !processed[endX + 1, startY]) endX++;
+        
+        if(endX > startX){
+            for(int x = startX; x <= endX; x++) processed[x, startY] = true;
+            return new WallSegment(new Vector2Int(startX, startY), new Vector2Int(endX, startY), true);
+        }
+        return null;
+    }
+    
+    static WallSegment CreateVerticalSegment(int startX, int startY, bool[,] wallMap, bool[,] processed, Vector2Int size){
+        int endY = startY;
+        while(endY + 1 < size.y && wallMap[startX, endY + 1] && !processed[startX, endY + 1]) endY++;
+        
+        if(endY > startY){
+            for(int y = startY; y <= endY; y++) processed[startX, y] = true;
+            return new WallSegment(new Vector2Int(startX, startY), new Vector2Int(startX, endY), false);
+        }
+        return null;
+    }
+    
+    static void CreateWallSegment(WallSegment segment, Transform parent, float wallHeight){
+        var wall = ShapeGenerator.CreateShape(ShapeType.Cube).gameObject;
+        wall.transform.parent = parent;
+        wall.GetComponent<MeshRenderer>().material = Instance.wallMat;
+        
+        if(segment.isHorizontal){
+            float length = segment.end.x - segment.start.x + 1;
+            wall.transform.position = new Vector3(segment.start.x + length * 0.5f - 0.5f, wallHeight * 0.5f, segment.start.y);
+            wall.transform.localScale = new Vector3(length, wallHeight, Instance.wallThickness);
+        }
+        else{
+            float length = segment.end.y - segment.start.y + 1;
+            wall.transform.position = new Vector3(segment.start.x, wallHeight * 0.5f, segment.start.y + length * 0.5f - 0.5f);
+            wall.transform.localScale = new Vector3(Instance.wallThickness, wallHeight, length);
+        }
+        meshes.Add(wall.GetComponent<ProBuilderMesh>());
+    }
+
+    static void MarkFloorAreas(Corridor node, bool[,] floorMap, Vector2Int size){
+        if(node == null) return;
+        if(node.IsLeaf && node.room != null) MarkFloorArea(node.room, floorMap, size);
+        else { MarkFloorAreas(node.left, floorMap, size); MarkFloorAreas(node.right, floorMap, size); }
+    }
+
+    static void MarkFloorArea(Room room, bool[,] floorMap, Vector2Int size){
+        for(int x = room.Pos.x; x < room.Pos.x + room.Size.x && x < size.x; x++)
+            for(int y = room.Pos.y; y < room.Pos.y + room.Size.y && y < size.y; y++)
+                if(x >= 0 && y >= 0) floorMap[x, y] = true;
+    }
+
+    static bool IsAdjacentToFloor(int x, int y, bool[,] floorMap, Vector2Int size){
+        return (x > 0 && floorMap[x-1, y]) || (x < size.x-1 && floorMap[x+1, y]) || 
+               (y > 0 && floorMap[x, y-1]) || (y < size.y-1 && floorMap[x, y+1]);
+    }
+
+    static bool IsBoundaryWall(int x, int y, bool[,] floorMap, Vector2Int size){
+        return (x == 0 || x == size.x-1 || y == 0 || y == size.y-1) && 
+               ((x > 0 && floorMap[x-1, y]) || (x < size.x-1 && floorMap[x+1, y]) || 
+                (y > 0 && floorMap[x, y-1]) || (y < size.y-1 && floorMap[x, y+1]));
+    }
+
+    //////////////////////
+    /// help functions /// : PostProcessMeshes
+    //////////////////////
+    
+    static void CombineObjects(){
+        var combinedMeshes = CombineMeshes.Combine(meshes);
+        if (combinedMeshes != null && combinedMeshes.Count > 0){
+            var combined = combinedMeshes[0];
+            combined.transform.SetPositionAndRotation(RoomGenerator.Instance.BaseMesh.transform.position, RoomGenerator.Instance.BaseMesh.transform.rotation);
+            combined.transform.localScale = RoomGenerator.Instance.BaseMesh.transform.localScale;
+            
+            var oldBaseMesh = RoomGenerator.Instance.BaseMesh;
+            RoomGenerator.Instance.BaseMesh = combined;
+            
+            if (oldBaseMesh != null ? oldBaseMesh.gameObject : null != null) DestroyImmediate(oldBaseMesh.gameObject);
+            foreach (var mesh in meshes) 
+                if (mesh != null ? mesh.gameObject : null != null && mesh != combined) DestroyImmediate(mesh.gameObject);
+            
+            combined.ToMesh();
+            combined.Refresh();
+            combined.transform.parent = RoomGenerator.Instance.transform;
+            var collider = combined.gameObject.AddComponent<MeshCollider>();
+            collider.sharedMesh = combined.GetComponent<MeshFilter>().mesh;
+        }
     }
 }
--- Assets/Scripts/RoomGeneratorClasses.cs
+++ Assets/Scripts/RoomGeneratorClasses.cs
@@ -1,284 +1,69 @@
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+using System.Drawing;
 using UnityEngine;
-using UnityEngine.UI;
-[Serializable]
-public class StarterCorridor : Corridor{
-    public static int ServerSpace;
-    public StarterCorridor(int x, int y, int width, int height){
-        Height = height;
-        Width = width;
-        this.x = x;
-        this.y = y;
-    }
-    public override (int x,int y,int width,int height) GetSpaceCorridorValues(){ return (firstRoomBudget+x,y,ServerSpace,Height); }
-    public override (int x,int y,int width,int height) GetFirstRoomValues(){ return (x,y,firstRoomBudget,Height); }
-    public override (int x,int y,int width,int height) GetSecondRoomValues(){ return (x+firstRoomBudget+ServerSpace,y,secondRoomBudget,Height); }
-    public override void PartitionCorridors(int depthLimit){
-        firstRoomBudget = (Width-ServerSpace)/2;
-        secondRoomBudget = Width - ServerSpace - firstRoomBudget;
-        var spaceVals = GetSpaceCorridorValues();
-        var firstVals = GetFirstRoomValues();
-        var secndVals = GetSecondRoomValues();
 
-        corridors = new(3){
-            new HorizontalCorridor(firstVals.x,firstVals.y,firstVals.width,firstVals.height),
-            new SpaceCorridor(spaceVals.x,spaceVals.y,spaceVals.width,spaceVals.height),
-            new HorizontalCorridor(secndVals.x,secndVals.y,secndVals.width,secndVals.height),
-        };
-
-        corridors[0].PartitionCorridors(depthLimit-1);
-        corridors[2].PartitionCorridors(depthLimit-1);
-    }
-}
-[Serializable]
-public abstract class Corridor{
-    public int Height;
-    public int Width;
-    // pivot point is bottom left
-    public int x;
-    public int y;
-    public int Budget;
-    public static int Space;
+[System.Serializable]
+public class Corridor{
+    // static fields
+    public static int MaxDepth;
     public static int MinSpace;
-    [SerializeReference] public List<Corridor> corridors;
-    public List<Door> doors = new();
 
-    // partition values
-    internal int firstRoomBudget;
-    internal int secondRoomBudget;
-    public Corridor parentCorridor;
-    public virtual void PartitionCorridors(int depthLimit){
-        if(depthLimit <= 0) {PartitionRoom();return;}
-        //precalc
-        Budget -= Space;
+    // corridor fields
+    public Corridor left, right;
+    // pivot point is bottom left
+    public Vector2Int Pos, Size;
+    public Room room;
+    public bool IsLeaf => left == null && right == null;
+    // hi staticvoid._.
+    public static void PartitionCorridors(Corridor node, int depth){
+        if (depth >= MaxDepth) return;
 
-        if(Budget <= MinSpace*2) {PartitionRoom();return;}
-
-        firstRoomBudget = UnityEngine.Random.Range(MinSpace, Budget-MinSpace+1);
-        secondRoomBudget = Budget - firstRoomBudget;
-        var spaceVals = GetSpaceCorridorValues();
-        var firstVals = GetFirstRoomValues();
-        var secndVals = GetSecondRoomValues();
-
-        corridors = new(3){
-            GetRoomType(firstVals.x,firstVals.y,firstVals.width,firstVals.height),
-            new SpaceCorridor(spaceVals.x,spaceVals.y,spaceVals.width,spaceVals.height),
-            GetRoomType(secndVals.x,secndVals.y,secndVals.width,secndVals.height)
-        };
-
-        foreach (var child in corridors) 
-            child.parentCorridor = this;
-
-        corridors[0].PartitionCorridors(depthLimit-1);
-        corridors[2].PartitionCorridors(depthLimit-1);
-    }
-    public virtual void PartitionRoom(){
-        int width2 = UnityEngine.Random.Range((MinSpace+Width)/2-Space, Width-Space);
-        int height2 = UnityEngine.Random.Range((MinSpace+Height)/2-Space, Height-Space);
-        int x2 = x+(Width-width2)/2;
-        int y2 = y+(Height-height2)/2;
-        corridors = new(1){
-            new Room(x2,y2,width2,height2)
-        };
-        ((Room)corridors[0]).Parent = this;
-    }
-    //help functions
-    public virtual (int x,int y,int width,int height) GetSpaceCorridorValues(){ return (0,0,0,0); }
-    public virtual (int x,int y,int width,int height) GetFirstRoomValues(){ return (0,0,0,0); }
-    public virtual (int x,int y,int width,int height) GetSecondRoomValues(){ return (0,0,0,0); }
-    private Corridor GetRoomType(int x,int y,int width,int height){
-        return width > height ? new VerticalCorridor(x,y,width,height) : new HorizontalCorridor(x,y,width,height);
-    }
-    public Corridor GetClosestChild(int x, int y, Corridor contendant = null,Corridor exclude = null){
-        if(corridors == null || corridors.Count != 3) return contendant; // nothing to return
-        contendant ??= corridors[0]; // set to our first child to compare with
-
-        if(corridors[0] != exclude && corridors[0].x <= contendant.x && corridors[0].y < contendant.y){ // the = in <= is important
-            contendant = corridors[0];
-        }else if(corridors[2] != exclude && corridors[2].x <= contendant.x && corridors[2].y < contendant.y){ // skip spaces at [1]
-            contendant = corridors[2];
-        }
-
-        contendant = corridors[0].GetClosestChild(x,y,contendant,exclude);
-        contendant = corridors[2].GetClosestChild(x,y,contendant,exclude);
-
-        return contendant;
-    }
-}
-[Serializable]
-public class VerticalCorridor : Corridor{
-    public VerticalCorridor(int x, int y, int width, int height){
-        Height = height;
-        Width = Budget = width;
-        this.x = x;
-        this.y = y;
-    }
-    // return values of the corridor based on THIS corridor
-    public override (int x,int y,int width,int height) GetSpaceCorridorValues(){ 
-        return (firstRoomBudget+x, y, Space, Height);
-    }
-    public override (int x,int y,int width,int height) GetFirstRoomValues(){ 
-        return (x, y, firstRoomBudget, Height);
-    }
-    public override (int x,int y,int width,int height) GetSecondRoomValues(){ 
-        return (x+firstRoomBudget+Space, y, secondRoomBudget, Height);
-    }
-}
-[Serializable]
-public class HorizontalCorridor : Corridor{
-    public HorizontalCorridor(int x, int y, int width, int height){
-        Width = width;
-        Height = Budget = height;
-        this.x = x;
-        this.y = y;
-    }
-    public override (int x,int y,int width,int height) GetSpaceCorridorValues(){ 
-        return (x, firstRoomBudget+y, Width, Space);
-    }
-    // below sounds horrible, but we actually need to reverse first & second rooms because we need the pivot point to be on bottom left, so we reverse the values of horizontal corridors
-    public override (int x,int y,int width,int height) GetFirstRoomValues(){ 
-        return (x, y+firstRoomBudget+Space, Width, secondRoomBudget);
-    }
-    public override (int x,int y,int width,int height) GetSecondRoomValues(){ 
-        return (x, y, Width, firstRoomBudget);
-    }
-}
-[Serializable]
-public class SpaceCorridor : Corridor{
-    public SpaceCorridor(int x, int y, int width, int height){
-        Height = height;
-        Width = width;
-        this.x = x;
-        this.y = y;
-    }
-}
-
-public class Room : Corridor{
-    public Corridor Parent;
-    public Room(int x, int y, int Width, int Height){
-        this.Width = Width;
-        this.Height = Height;
-        this.x = x;
-        this.y = y;
-    }
-}
-
-public class Door{ // not related to corridor class
-    public static List<Door> staticDoors = new();
-    public int x;
-    public int y;
-    public int Width;
-    public int Height;
-    public static int baseLength = 3; // for vertical/horizontal length in the other direction
-    public Room[] ConnectedRooms;
-    
-    public static bool ConnectionExists(Room room1, Room room2){
-        if(room1 == null && room2 == null){
-            Debug.Log("invalid rooms passed");
-            return false;
-        }
-        return staticDoors.Any(x => x.ConnectedRooms != null && x.ConnectedRooms[0] != null && x.ConnectedRooms[1] != null && x.ConnectedRooms[0] == room1 && x.ConnectedRooms[1] == room2);
-    }
-    
-    public static void ConnectRooms(Corridor corridor){
-        if (corridor == null) return;
+        float ratio = (float)node.Size.x / node.Size.y;
+        bool splitH = ratio <= 1.25f && (ratio < 0.8f || Random.value > 0.5f);
         
-        if (corridor.corridors != null){
-            foreach (var child in corridor.corridors)
-                ConnectRooms(child);
-        }
+        int max = (splitH ? node.Size.y : node.Size.x) - MinSpace;
+        if(max <= MinSpace) return;
 
-        if (corridor.corridors?.Count == 3){
-            var leftChild = corridor.corridors[0];
-            var rightChild = corridor.corridors[2];
-            var spaceChild = corridor.corridors[1];
+        int split = Random.Range(MinSpace, max);
+
+        if(splitH){
+            node.left = new Corridor { Pos = node.Pos, Size = new(node.Size.x, split) };
+            node.right = new Corridor { Pos = node.Pos + new Vector2Int(0, split), Size = new(node.Size.x, node.Size.y - split) };
+        } else {
+            node.left = new Corridor { Pos = node.Pos, Size = new(split, node.Size.y) };
+            node.right = new Corridor { Pos = node.Pos + new Vector2Int(split, 0), Size = new(node.Size.x - split, node.Size.y) };
+        }
+        
+        PartitionCorridors(node.left, depth + 1);
+        PartitionCorridors(node.right, depth + 1);
+    }
+    public static void CreateRooms(Corridor node){
+        if(node.IsLeaf){
+            int roomWidth = Random.Range(node.Size.x / 2, node.Size.x - 2);
+            int roomHeight = Random.Range(node.Size.y / 2, node.Size.y - 2);
+            int roomX = node.Pos.x + Random.Range(1, node.Size.x - roomWidth);
+            int roomY = node.Pos.y + Random.Range(1, node.Size.y - roomHeight);
             
-            var leftRoom = GetAnyRoom(leftChild);
-            var rightRoom = GetAnyRoom(rightChild);
-
-            if (leftRoom != null && rightRoom != null && !ConnectionExists(leftRoom, rightRoom)){
-                if (corridor is VerticalCorridor)
-                    ConnectVertical(leftRoom, rightRoom, spaceChild);
-                else if (corridor is HorizontalCorridor) 
-                    ConnectHorizontal(leftRoom, rightRoom, spaceChild);
-            }
+            node.room = new Room { Pos = new Vector2Int(roomX, roomY), Size = new Vector2Int(roomWidth, roomHeight) };
+        } else {
+            CreateRooms(node.left);
+            CreateRooms(node.right);
         }
-    }
-    
-    private static Room GetAnyRoom(Corridor corridor){
-        if (corridor is Room room) 
-            return room;
-            
-        if (corridor.corridors?.Count > 0){
-            foreach(var child in corridor.corridors){
-                var foundRoom = GetAnyRoom(child);
-                if(foundRoom != null) return foundRoom;
-            }
-        }
-        
-        return null;
-    }
-
-
-    private static void ConnectVertical(Room left, Room right, Corridor spaceChild) {
-        int minY = Math.Max(left.y, right.y);
-        int maxY = Math.Min(left.y + left.Height, right.y + right.Height);
-
-        if (minY >= maxY) return;
-        if(ConnectionExists(left, right)) return;
-        
-        int doorY = (minY + maxY) / 2;
-        int doorX = spaceChild.x + spaceChild.Width / 2;
-        var leftroomclosest = spaceChild.parentCorridor.GetClosestChild(doorX,doorY+Corridor.MinSpace);
-        var rightroomclosest = spaceChild.parentCorridor.GetClosestChild(doorX,doorY+Corridor.MinSpace, null, leftroomclosest);
-
-        if(leftroomclosest == rightroomclosest){
-            Debug.Log("huh??? that cant happen");
-            return;
-        }
-
-        int width = (leftroomclosest.x+leftroomclosest.Width+rightroomclosest.x)/2;
-        
-        right.doors.Add(new Door{ 
-            x = doorX, 
-            y = doorY,
-            Height=baseLength,
-            Width=width,
-            ConnectedRooms = new[]{left, right} 
-        });
-        staticDoors.Add(right.doors.Last());
-    }
-
-    private static void ConnectHorizontal(Room bottom, Room top, Corridor spaceChild) {
-        int minX = Math.Max(bottom.x, top.x);
-        int maxX = Math.Min(bottom.x + bottom.Width, top.x + top.Width);
-        
-        if (minX >= maxX) return;
-        if(ConnectionExists(bottom, top)) return;
-        
-        int doorX = (minX + maxX) / 2;
-        int doorY = spaceChild.y + spaceChild.Height / 2;
-        var uproomclosest = spaceChild.parentCorridor.GetClosestChild(doorX-Corridor.MinSpace,doorY);
-        var downroomclosest = spaceChild.parentCorridor.GetClosestChild(doorX+Corridor.MinSpace,doorY, null, uproomclosest);
-
-        if(uproomclosest == downroomclosest){
-            Debug.Log("huh??? that cant happen");
-            return;
-        }
-
-        int height = (downroomclosest.x+downroomclosest.Width+uproomclosest.x)/2;
-        
-        top.doors.Add(new Door{ 
-            x = doorX, 
-            y = doorY,
-            Width=baseLength,
-            Height=height,
-            ConnectedRooms = new[]{bottom, top} 
-        });
-        staticDoors.Add(top.doors.Last());
     }
 }
+public class Room{ // no longer a child of corridor
+    public Vector2Int Pos, Size;
+}
+public class WallSegment{
+    public Vector2Int start;
+    public Vector2Int end;
+    public bool isHorizontal;
+    
+    public WallSegment(Vector2Int start, Vector2Int end, bool isHorizontal)
+    {
+        this.start = start;
+        this.end = end;
+        this.isHorizontal = isHorizontal;
+    }
+}
+// old code used to be.... WAY longer.
